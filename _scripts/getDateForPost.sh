744675fa786ecceeae08b1c38ae34902c91075b9
Chris Bishop <cjbishop1986@gmail.com>
2021-12-14 04:43:12 -0500

    init commit of ATR GHpages site to Org - 4

--- _scripts/getDateForPost.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# produces Jekyll blog post compliant datetime stamp output like the following, with time in 24hr format and -500 representing NY USA EST UTC offset
-# 2021-12-10 21:09:55 -0500
-# See: https://www.cyberciti.biz/faq/linux-unix-formatting-dates-for-display/
-
-NOW=`date +"%Y-%m-%d %T %z"`
-
-echo $NOW
